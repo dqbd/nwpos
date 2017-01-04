d1b053eb944b2d36c57e093cfb0425ad710ba9c8
David Duong <david@duong.cz>
2017-01-04 18:57:37 +0100

    pi

--- server/customer/app.py
+++ server/customer/app.py
@@ -11,33 +11,37 @@ class pitft:
 	screen = None;
 
 	def __init__(self):
-		# "Ininitializes a new pygame screen using the framebuffer"
+		"Ininitializes a new pygame screen using the framebuffer"
 		# Based on "Python GUI in Linux frame buffer"
 		# http://www.karoltomala.com/blog/?p=679
 		disp_no = os.getenv("DISPLAY")
 		if disp_no:
 			print "I'm running under X display = {0}".format(disp_no)
-
-		os.putenv('SDL_FBDEV', '/dev/fb1')
-
-		# Select frame buffer driver
-		# Make sure that SDL_VIDEODRIVER is set
-		driver = 'fbcon'
-		if not os.getenv('SDL_VIDEODRIVER'):
-			os.putenv('SDL_VIDEODRIVER', driver)
-		try:
-			pygame.display.init()
-		except pygame.error:
-			print 'Driver: {0} failed.'.format(driver)
-			exit(0)
-
+		
+		# Check which frame buffer drivers are available
+		# Start with fbcon since directfb hangs with composite output
+		drivers = ['fbcon', 'directfb', 'svgalib']
+		found = False
+		for driver in drivers:
+			# Make sure that SDL_VIDEODRIVER is set
+			if not os.getenv('SDL_VIDEODRIVER'):
+				os.putenv('SDL_VIDEODRIVER', driver)
+			try:
+				pygame.display.init()
+			except pygame.error:
+				print 'Driver: {0} failed.'.format(driver)
+				continue
+			found = True
+			break
+	
+		if not found:
+			raise Exception('No suitable video driver found!')
+		
 		size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-		
 		print "Framebuffer size: %d x %d" % (size[0], size[1])
-		
 		self.screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 		# Clear the screen to start
-		self.screen.fill((0, 0, 0))
+		self.screen.fill((0, 0, 0))		
 		# Initialise font support
 		pygame.font.init()
 		# Render the screen
