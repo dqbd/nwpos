f69bbdebb4e5a0cf66a494648fe75e6bcb966f30
David Duong <david@duong.cz>
2017-01-06 17:50:19 +0100

    ui tweaks

--- client/components/TotalButton.jsx
+++ client/components/TotalButton.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Total = ({total, onCheckout}) => {
-	return <span key="total" className="btn color total"><a onTouchTap={onCheckout}>Suma {total} Kč</a></span>
+	return <span key="total" className="btn color total"><a onTouchTap={onCheckout}>Celkem {total} Kč</a></span>
 }
 
 export default Total
