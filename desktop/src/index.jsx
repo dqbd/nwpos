1bbd2b9ed53ab9c32f8200dbe151ed3c2e19751c
David Duong <david@duong.cz>
2017-02-07 19:11:23 +0100

    fix desktop load

--- desktop/src/index.jsx
+++ desktop/src/index.jsx
@@ -3,7 +3,7 @@ require("./style.styl")
 import React from "react"
 import { render } from "react-dom"
 
-import Setup from "./steps/Setup.jsx"
+import Setup from "./setup/Setup.jsx"
 
 render(<Setup />, document.getElementById("root"))
 
