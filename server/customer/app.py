c07875a3b995360b4d72d66cefd1cc1ec86fb2e9
David Duong <david@duong.cz>
2017-02-13 21:25:32 +0100

    non root customer

--- server/customer/app.py
+++ server/customer/app.py
@@ -3,6 +3,10 @@ import sys, os, json, pygame
 from threading import Thread
 from Queue import Queue, Empty
 
+os.putenv('SDL_FBDEV', '/dev/fb0')
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
+os.putenv('SDL_NOMOUSE', '1')
+
 size = width, height = 1280, 1024
 
 def enqueue_output(stdin, queue):
