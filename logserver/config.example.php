e93bfc6725d4bd6d24d38115ff4d7c006ad2d565
David Duong <david@duong.cz>
2017-02-17 20:59:37 +0100

    server config & db

---
+++ logserver/config.example.php
@@ -0,0 +1,6 @@
+<?php
+    $DB_HOST = "";
+    $DB_NAME = "";
+    $DB_USER = "";
+    $DB_PASS = "";
+?>
--- logserver/index.php
+++ logserver/index.php
@@ -1,2 +1,10 @@
 <?php
+    require_once("config.php");
+    $pdo = new PDO("mysql:dbname=$DB_NAME;host=$DB_HOST", $DB_USER, $DB_PASS);
+
+    if ($_GET["query"] === "client") {
+
+    } else if ($_GET["query"] === "log") {
+
+    }
 ?>
