0d2661a2f221f5ef3d596f76036585ff6de03435
David Duong <david@duong.cz>
2017-05-24 22:13:05 +0200

    summary of month

--- server/database/logs.js
+++ server/database/logs.js
@@ -66,6 +66,53 @@ class Logs extends Database {
 		}))
 	}
 
+	retrieveSummary() {
+		return this.getLogList().then(days => {
+			let mappings = {}
+
+			for (let day of days) {
+				let [date, month, year] = day.split(".")
+				let tag = month + "." + year
+				if (!mappings[tag]) mappings[tag] = {days: 0, list: []}
+
+				mappings[tag].list.push(day)
+				mappings[tag].days++ 
+			}
+
+			return mappings
+		}).then(maps => {
+			return this.getDb("summary").then(db => {
+				let final = Promise.resolve([])
+				Object.keys(maps).forEach((season) => {
+					let { days, list } = maps[season]
+
+					final = final.then((result) => new Promise((resolve, reject) => {
+						db.find({ period: season, days: { $gte: days } }, (err, data) => {
+							if (err) return reject(err)
+							if (data.length > 0) return resolve([...result, data[0]])
+
+							// calculate sum
+							let summer = Promise.resolve(0)
+							for (let date of list) {
+								summer = summer.then((total) => this.retrieveLog(date).then(log => total + log.total))
+							}
+							
+							summer.then(total => {
+								// NOTE: returnUpdatedDocs is incompatible with MongoDB
+								db.update({ period: season }, { period: season, days, total }, { upsert: true, returnUpdatedDocs: true }, (err, num, affected) => {
+									if (err) return reject(err)
+									resolve([...result, affected])
+								})
+							})
+						})	
+					}))
+				})
+
+				return final
+			})
+		})
+	}
+
 	updateLog(date, id, data) {
 		return this.getDb(date).then(db => new Promise((resolve, reject) => {
 			db.update({ _id: id }, data, (err, updated) => {
