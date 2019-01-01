a8825c3d0be0702d72ecc0b89f870ce87741e154
David Duong <david@duong.cz>
2019-01-01 19:09:50 +0100

    Move scanner disable to actions

--- client/components/Tabs.jsx
+++ client/components/Tabs.jsx
@@ -58,15 +58,11 @@ export default class Tabs extends Component {
                 </svg>
                 <span className={["status", nativePrinter ? "active" : "inactive"].join(" ")} />
             </a> }
-            <a className={["scanner", listenToScanner ? 'active' : 'inactive'].join(' ')} onTouchTap={() => onScannerSet(!listenToScanner)}>
-                { listenToScanner && <svg viewBox='0 0 24 24'>
-                    <path d='M4,6H6V18H4V6M7,6H8V18H7V6M9,6H12V18H9V6M13,6H14V18H13V6M16,6H18V18H16V6M19,6H20V18H19V6M2,4V8H0V4A2,2 0 0,1 2,2H6V4H2M22,2A2,2 0 0,1 24,4V8H22V4H18V2H22M2,16V20H6V22H2A2,2 0 0,1 0,20V16H2M22,20V16H24V20A2,2 0 0,1 22,22H18V20H22Z' />
-                </svg> }
-
-                { !listenToScanner && <svg viewBox='0 0 24 24'>
+            { !listenToScanner && <a className="scanner inactive" onTouchTap={() => onScannerSet(!listenToScanner)}>
+                <svg viewBox='0 0 24 24'>
                     <path d='M2,6H4V18H2V6M5,6H6V18H5V6M7,6H10V18H7V6M11,6H12V18H11V6M14,6H16V18H14V6M17,6H20V18H17V6M21,6H22V18H21V6Z' />
-                </svg> }
-            </a>
+                </svg>
+            </a> }
         </div>
     }
 }
--- client/utils/actions.js
+++ client/utils/actions.js
@@ -3,6 +3,8 @@ import Dashboard from "../pages/Dashboard.jsx"
 import Suggestions from "../pages/Suggestions.jsx"
 import Config from "../pages/Config.jsx"
 
+import { seller } from "../../core"
+
 let toggleNight = function(ignore = false) {
 	let body = document.querySelector("body")
 	let value = JSON.parse(window.localStorage.getItem("dark"))
@@ -25,12 +27,20 @@ let toggleNight = function(ignore = false) {
 	window.localStorage.setItem("dark", value)
 }
 
+let toggleScanner = function() {
+	const { store } = this
+	if (store) {
+		store.dispatch(seller.setListenToScanner(!store.getState().listenToScanner))
+	}
+}
+
 let actions = [
 	{ func: "showClient", name: "Přejít k pokladně", impl: Main.show },
 	{ func: "showStats", name: "Zobrazit statistiky", impl: Dashboard.show },
 	{ func: "showConfig", name: "Konfigurace serveru", impl: Config.show },
 	{ func: "showSuggestions", name: "Zobrazit našeptávaní", impl: Suggestions.show },
-	{ func: "toggleNight", name: "Přepnout tmavý režim", impl: toggleNight }
+	{ func: "toggleNight", name: "Přepnout tmavý režim", impl: toggleNight },
+	{ func: "toggleScanner", name: "Přepnout globální skener", impl: toggleScanner }
 ]
 
 module.exports.bindActions = (store) => {
