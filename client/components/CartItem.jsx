c6db0ae99e896655a8fee14da8de883e20c19cec
David Duong <david@duong.cz>
2019-01-01 15:54:43 +0100

    add info if the item is scanned

--- client/components/CartItem.jsx
+++ client/components/CartItem.jsx
@@ -22,12 +22,21 @@ class CartItem extends Component {
 	}
 
 	render() {
-		let { index, price, qty, name, selected, onSelect, onRename, onAddQty, onDelete } = this.props
+		let { index, price, qty, name, selected, ean, onSelect, onRename, onAddQty, onDelete } = this.props
 		
 		return (<li onTouchTap={() => onSelect(index)} className={selected ? "item selected" : "item"}>
 				{this.state.showEditName ? <RenamePopup price={price} current={name} onEdit={(e) => this.onFormRename(e)} onClose={() => this.onFormClose()} /> : null}
 				<span className="digit inline-btn" onTouchTap={() => onDelete(index)}></span>
-				<span className="name" onTouchTap={() => this.onFormOpen()}>{name}</span>
+				<span className="name" onTouchTap={() => this.onFormOpen()}>
+					{name}
+					{ean && (
+						<span className="ean">
+							<svg viewBox='0 0 24 24'>
+								<path d='M2,6H4V18H2V6M5,6H6V18H5V6M7,6H10V18H7V6M11,6H12V18H11V6M14,6H16V18H14V6M17,6H20V18H17V6M21,6H22V18H21V6Z' />
+							</svg>
+						</span>
+					)}
+				</span>
 				<span className="price">{price}</span>
 				<span className="qty">
 					<span className="inline-btn" onTouchTap={() => onAddQty(-1, index)}>&#8722;</span>
